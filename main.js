e14cfa246d01012e26f9c651f76401bf23d1c358
Alex Lopez <alexlopezm877@gmail.com>
2026-04-26 07:43:30 -0600

    all

--- main.js
+++ main.js
@@ -31,3 +31,53 @@ function logout() {
   localStorage.removeItem("user");
   location.reload();
 }
+let products = JSON.parse(localStorage.getItem("products")) || [];
+
+function addProduct() {
+  const p = {
+    name: name.value,
+    price: price.value,
+    stock: stock.value,
+    img: img.value || "https://via.placeholder.com/150"
+  };
+
+  products.push(p);
+  save();
+  render();
+}
+
+function save() {
+  localStorage.setItem("products", JSON.stringify(products));
+}
+
+function render() {
+  const container = document.getElementById("products");
+  if (!container) return;
+
+  container.innerHTML = "";
+
+  products.forEach((p, i) => {
+    container.innerHTML += `
+        <div class="card-product">
+            <img src="${p.img}">
+            <h4>${p.name}</h4>
+            <p class="price">$${p.price}</p>
+            <p class="${p.stock < 5 ? 'low' : ''}">
+                Stock: ${p.stock}
+            </p>
+            <button class="delete" onclick="removeProduct(${i})">
+                Eliminar
+            </button>
+        </div>
+        `;
+  });
+}
+
+function removeProduct(i) {
+  products.splice(i, 1);
+  save();
+  render();
+}
+
+/* cargar */
+setTimeout(render, 500);
